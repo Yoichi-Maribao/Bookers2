8f0591cb36bf64ba5b999d8cc5c48160b0cce6dd
Yoichi-Maribao <suzaki.yoichi.infratop@gmail.com>
2021-01-04 15:39:17 +0000

    error 4 nokori

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/about', to:'homes#about'
+  devise_for :users
+  root to: 'homes#top'
+  resources :books, only: [:index, :create, :show, :destroy, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 end
