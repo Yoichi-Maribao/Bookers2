77e8853f6abb440ddb01fb36fc5b76d3f4f45d5b
Yoichi-Maribao <suzaki.yoichi.infratop@gmail.com>
2021-01-07 16:27:37 +0000

    added comments and favorites

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'home/about', to:'homes#about'
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:index, :create, :show, :destroy, :edit, :update]
+  resources :books, only: [:index, :create, :show, :destroy, :edit, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
 end
