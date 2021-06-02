4eba2326fe0eaa91e9b8cc5004d34d450d512fb2
Yoichi-Maribao <szk.m.yo1@gmail.com>
2021-06-02 07:00:19 +0100

    deploy settings for AWS

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,8 +14,6 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many  :tag_relationships, dependent: :destroy
-  has_many  :tags, through: :tag_relationships
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
